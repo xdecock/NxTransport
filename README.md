34a65fe29a549c41a12a702e72208e8ccf2450b4
xdecock <xdecock@gmail.com>
2014-03-29 18:17:46 +0100

    Response Handler Coded (+ Handler for ENHANCEDSTATUSCODE)

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ NxTransport, an alternative ESMTP transport layer, depending on swiftmailer to c
 
 This aims to implement an event based transport layer, the implementation is not the purpose of this library, it's goal is to mess with implementation, and still keep swiftmailer for the mail composition.
 
-Swiftmailer ESMTP Plugins will still be supported with added benefits of DSN, SIZE=, ...
+Swiftmailer ESMTP Plugins will still be supported with added benefits of DSN, SIZE=, PIPELINING
--- lib/NxTransport/Protocol/ESMTP/Response/BasicHandler.php
+++ lib/NxTransport/Protocol/ESMTP/Response/BasicHandler.php
@@ -1,6 +1,64 @@
 <?php
 namespace NxTransport\Protocol\ESMTP\Response;
 
-class EnhancedHandler
+use NxTransport\Connection;
+
+class BasicHandler
 {
+
+    public $statusCode = 0;
+
+    public $response = '';
+
+    public $responseString = '';
+
+    public $finishedParsing = false;
+
+    public function __construct(array $expectedResponseCode)
+    {}
+
+    /**
+     * Parse a reply, prepare the statusCode
+     *
+     * @param Connection $connection
+     * @throws Exception
+     * @return boolean finished
+     */
+    public function parseReply(Connection $connection)
+    {
+        $this->responseString .= $this->parseLine($connection);
+        return $this->finishedParsing;
+    }
+
+    /**
+     * Parse a reply and return
+     *
+     * @param unknown $line
+     * @throws Exception
+     */
+    protected function parseLine(Connection $connection)
+    {
+        $line = $connection->readLine();
+        $this->response .= $line;
+        // Extract Status Code
+        $status = substr($line, 0, 3);
+        if (!is_numeric($status)) {
+            throw new Exception('Invalid Reply, line not starting with a 3 char digit status code');
+        }
+        if ($this->statusCode == 0) {
+            // First line of the reply
+            $this->statusCode = ((int) $status);
+        } elseif ($this->statusCode != $status) {
+            // Next Lines
+            throw new Exception('Invalid Multiline Reply, status code has changed between 2 lines');
+        }
+        if ($line[3] == ' ') {
+            // Last Line of the response
+            $finishedParsing = true;
+        } elseif ($line[3] != '-') {
+            // Invalid Separator
+            throw new Exception('Separator is invalid - or <SP> expected, ' . $line[3] . ' received');
+        }
+        return substr($line, 4, - 2);
+    }
 }
